11200734dc83aba4381dc45db703dc0029828632
techieTrial <yash.sjoshi18@gmail.com>
2021-11-18 22:10:27 +0530

    Remade design and added "Our Story"

--- ValGotKul/js/scripts.js
+++ ValGotKul/js/scripts.js
@@ -82,3 +82,20 @@ var x = setInterval(function() {
     document.getElementById("countdown").innerHTML = "Happily Married";
   }
 }, 1000);
+
+const square = document.querySelector('.square');
+square.classList.remove('square-transition');
+
+// Create the observer, same as before:
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      square.classList.add('square-transition');
+      return;
+    }
+
+    square.classList.remove('square-transition');
+  });
+});
+
+observer.observe(document.querySelector('.square-wrapper'));
